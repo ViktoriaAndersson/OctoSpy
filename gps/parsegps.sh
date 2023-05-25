87314f8a467a33ceda7fc0f119d6896dd7089ccb
Viktoria Andersson <viktoria123.andersson@gmail.com>
2023-05-25 15:25:11 +0100

    sleep

--- gps/parsegps.sh
+++ gps/parsegps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-while
-
+while :
+do
   SAMPLE=$(cat /dev/ttyS0 | grep -m 1 GNGGA)
 
   LATRAW=$(echo $SAMPLE | cut -d ',' -f 3)
@@ -10,15 +10,17 @@ while
   LATFRACTION=$(echo $LATRAW | cut -c 3-)
   LATDECIMAL=$(echo "scale=10; $LATFRACTION / 60" | bc)
 
-  echo $SAMPLE
-  echo $LATMIN
-  echo $LATFRACTION
-  echo $LATDECIMAL
-  echo $LATMIN$LATDECIMAL
 
+  LONGRAW=$(echo $SAMPLE | cut -d ',' -f 5)
+  LONGMIN=$(echo $LONGRAW | cut -c 1-3)
+  LONGFRACTION=$(echo $LONGRAW | cut -c 3-)
+  LONGDECIMAL=$(echo "scale=10; $LONGFRACTION / 60" | bc)
 
-  scp
+  echo "$(date +%s) $LATMIN$LATDECIMAL $LONGMIN$LONGDECIMAL" > coordinate.txt
+	
+  scp -P 2678 coordinate.txt group16@quorkel.com:http/
+  ssh -p 2678 group16@quorkel.com 'cat http/coordinate.txt >> http/coordinates.txt'
 
-  sleep 1
+  sleep 0.1
 
-end
+done
--- temperature/parsetemperature.sh
+++ temperature/parsetemperature.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+while :
+do
+  RAWTEMP=$(cat /sys/bus/w1/devices/28-00000e9547eb/w1_slave | grep "t=" | cut -d'=' -f2)
+  TEMP=$(echo $RAWTEMP | awk '{print $1 / 1000}')
+  echo "$(date +%s) $TEMP" > temperature.txt
 
+  scp -P 2678 temperature.txt group16@quorkel.com:http/
+  ssh -p 2678 group16@quorkel.com 'cat http/temperature.txt >> http/temperatures.txt'
 
+  sleep 0.1
+done
