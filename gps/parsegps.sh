4d5dd55bffe143f0a7304fd85395ed92b15ddd58
Viktoria Andersson <viktoria123.andersson@gmail.com>
2023-05-26 10:22:30 +0100

    update coordinate

--- gps/parsegps.sh
+++ gps/parsegps.sh
@@ -12,10 +12,12 @@ do
 
 
   LONGRAW=$(echo $SAMPLE | cut -d ',' -f 5)
-  LONGMIN=$(echo $LONGRAW | cut -c 1-3)
-  LONGFRACTION=$(echo $LONGRAW | cut -c 3-)
+  LONGMIN=$(echo $LONGRAW | cut -c 2-3)
+  LONGFRACTION=$(echo $LONGRAW | cut -c 4-)
   LONGDECIMAL=$(echo "scale=10; $LONGFRACTION / 60" | bc)
 
+  echo "$LATMIN$LATDECIMAL"
+  echo "$LONGMIN$LONGDECIMAL"
   echo "$(date +%s) $LATMIN$LATDECIMAL $LONGMIN$LONGDECIMAL" > coordinate.txt
 	
   scp -P 2678 coordinate.txt group16@quorkel.com:http/
